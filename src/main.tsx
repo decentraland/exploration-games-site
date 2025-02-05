0f6b0f6b7be539dbebe9f1fa4b94d2b874fab470
Braian Mellor <braianj@gmail.com>
2025-02-05 10:20:48 -0300

    pr related

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,12 @@ import {
   darkTheme,
 } from "decentraland-ui2"
 import { router } from "./components/Router/Router"
-import { config } from "./config"
-
-const ssoUrl = config.get("SSO_URL")
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <CssVarsProvider theme={darkTheme}>
     <CssBaseline />
     {/* @ts-expect-error AuthProvider type definition issue */}
-    <AuthProvider sso={ssoUrl}>
+    <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
   </CssVarsProvider>
